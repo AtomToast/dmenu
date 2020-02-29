ce6a8fa152caec6bb2eedf1058ff187b9364ab26
AtomToast <reg-2@t-online.de>
2020-02-29 22:00:45 +0100

    add monokai colors

--- config.def.h
+++ config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#f8f8f2", "#282828" }, // { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#383830", "#f92672" }, // { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#66d9ef" }, // { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
