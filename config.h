5ba1d10eadbb221fe804daf3a6f717823bb6e5e2
AtomToast <reg-2@t-online.de>
2020-04-29 01:34:20 +0200

    remove config.h from .gitignore

---
+++ config.h
@@ -0,0 +1,24 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Hack Nerd Font:pixelsize=14:antialias=true:autohint=true",
+	"Joy Pixels:pixelsize=14:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f8f8f2", "#282828" }, // { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#383830", "#f92672" }, // { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#66d9ef" }, // { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
